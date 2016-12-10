18d2d85a4a118388560a04fcbd5d7683e1094a5e
Muhammadreza Haghiri <haghiri75@gmail.com>
2016-12-10 16:50:25 +0330

    Hours problem solved

---
+++ clock/clock.ino
@@ -0,0 +1,36 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+int sec = 0;  
+int minute = 48; 
+int hour = 16; 
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("My Clock!");
+}
+
+void loop() {
+ lcd.setCursor(0, 1);
+ lcd.print(" ");
+ if(hour > 23){
+  hour = 0; 
+ }
+ lcd.print(hour);
+ lcd.print(" : ");
+  lcd.print(minute);
+ if(minute > 59){
+  minute = 0; 
+  hour += 1;
+ }
+ lcd.print(" : ");
+ lcd.print(sec);
+ delay(1000);
+ sec += 1;
+ if(sec > 59){
+  sec = 0; 
+  minute += 1;
+ }
+ lcd.print("    ");
+ 
+}
